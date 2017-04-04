160e331fdd2b016c622035c64b3f17a0c963cbad
HeuiChan(Terrencce) Lim <hlim1@mail.csuchico.edu>
2017-04-04 11:16:09 -0700

    lab completed

--- groceries.js
+++ groceries.js
@@ -1,3 +1,17 @@
+function addItem()
+{
+  var input    = document.getElementById("newItem").value;
+  var list     = document.getElementById("listDisplay");
+  var item     = document.createElement("li"); // Create a <li> node
+  var itemName = document.createTextNode(input); //create a text node with the value of input variable
+
+  item.appendChild(itemName); // Append itemName to <li> item
+  list.appendChild(item); // Append item to <ul> list
+
+  // Setting the value to the empty string so 
+  //that what the user typed in is no longer shown in the text box
+  document.getElementById("newItem").value = ""; 
+}
 
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
 function setCookie(cname, cvalue, exdays) {
